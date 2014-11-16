ca654b9fd0eb711ecd79e4f0158a760bc22f3763
David Wen <dstwen@gmail.com>
2014-11-16 12:28:41 -0800

    make fetchDreams call

--- SixDegrees/SixDegrees/View Controllers/Dream/DreamViewController.m
+++ SixDegrees/SixDegrees/View Controllers/Dream/DreamViewController.m
@@ -7,15 +7,47 @@
 //
 
 #import "DreamViewController.h"
+#import "SDApiManager.h"
 
 @interface DreamViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *personalDreamLabel;
 @property (weak, nonatomic) IBOutlet UILabel *careerDreamLabel;
+@property (strong, nonatomic) SDApiManager *apiManager;
 
 @end
 
 @implementation DreamViewController
+
++ (BSPropertySet *)bsProperties {
+    BSPropertySet *propertySet = [BSPropertySet propertySetWithClass:self propertyNames:@"apiManager", nil];
+    [propertySet bindProperty:@"apiManager" toKey:[SDApiManager class]];
+    return propertySet;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self fetchDreams];
+}
+
+#pragma mark - IBAction
+
+- (void)fetchDreams {
+    [self.apiManager fetchDreams:@"Dream"
+                        success:^(NSArray *responseObject){
+                            [self updateDream:responseObject[0]];
+                            NSLog(@"JSON: %@", responseObject);
+                        } failure:^(NSError *error) {
+                            [self showAlertViewWithTitle:@"Failure!" message:error.description];
+                        }];
+}
+- (void)showAlertViewWithTitle:(NSString *)title message:(NSString *)message {
+    [[[UIAlertView alloc] initWithTitle:title
+                                message:message
+                               delegate:nil
+                      cancelButtonTitle:@"OK"
+                      otherButtonTitles:nil] show];
+}
 
 @end
