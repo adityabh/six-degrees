f6f69661991310cea53cf02cb3adb423aaa89c0e
David Wen <dstwen@gmail.com>
2014-11-16 14:08:22 -0800

    nextDream and incrementCurrentDream methods

--- SixDegrees/SixDegrees/View Controllers/Dream/DreamViewController.m
+++ SixDegrees/SixDegrees/View Controllers/Dream/DreamViewController.m
@@ -14,6 +14,8 @@
 @property (weak, nonatomic) IBOutlet FBProfilePictureView *profileImageView;
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *dreamLabel;
+@property (strong, nonatomic) NSArray *dreams;
+@property (assign) int currentDream;
 
 @property (strong, nonatomic) SDApiManager *apiManager;
 
@@ -37,7 +39,9 @@
 - (void)fetchDreams {
     [self.apiManager fetchDreams:@"Dream"
                         success:^(NSArray *responseObject){
-                            [self updateDream:responseObject[0]];
+                            self.dreams = responseObject;
+                            self.currentDream = 0;
+                            [self updateDream:self.dreams[self.currentDream]];
                             NSLog(@"JSON: %@", responseObject);
                         } failure:^(NSError *error) {
                             [self showAlertViewWithTitle:@"Failure!" message:error.description];
@@ -49,6 +53,20 @@
     self.dreamLabel.text = dream[@"content"][@"description"];
     self.profileImageView.profileID = dream[@"user"][@"uid"];
     
+}
+
+- (IBAction)nextDream:(id)sender {
+    [self incrementCurrentDream];
+    [self updateDream:self.dreams[self.currentDream]];
+}
+
+- (void)incrementCurrentDream {
+    int numDreams = [self.dreams count] - 1;
+    if (self.currentDream == numDreams) {
+        self.currentDream = 0;
+    } else {
+        self.currentDream += 1;
+    }
 }
 
 - (void)showAlertViewWithTitle:(NSString *)title message:(NSString *)message {
